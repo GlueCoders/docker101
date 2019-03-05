d12e27798f7c0bba1934a0ebc34e3be878828169
Anand Rajneesh <anandrajneesh@outlook.com>
2019-03-05 06:59:15 +0530

    Two springboot applications to test docker

---
+++ docker101/foo1/src/main/java/com/gluecoders/docker101/rest/Ping.java
@@ -0,0 +1,35 @@
+package com.gluecoders.docker101.rest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+public class Ping {
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @Value("${url}")
+    private String url;
+
+    @GetMapping(path = "/ping")
+    public ResponseEntity ping(){
+        return ResponseEntity.ok("All ok");
+    }
+
+
+    @GetMapping(path = "/pingother")
+    public ResponseEntity pingOther(){
+        try {
+            String response = restTemplate.getForObject(url, String.class);
+            return ResponseEntity.ok(response);
+        }catch (Exception e){
+            e.printStackTrace();
+            return ResponseEntity.status(500).body(e.getMessage());
+        }
+    }
+}
